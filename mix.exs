17eee3ba6e010f5716dea3e5939b0d0e0b4c0f11
ryantaylor <ryan.taylor9@gmail.com>
2019-10-26 14:16:17 -0400

    Update deps and lockfile.

--- mix.exs
+++ mix.exs
@@ -38,8 +38,8 @@ defmodule Manager.MixProject do
       "lib",
       "test/support",
       "test/factories",
-      "/Users/ryantaylor/Code/invest/persistence/test/support",
-      "/Users/ryantaylor/Code/invest/persistence/test/factories"
+      "deps/persistence/test/support",
+      "deps/persistence/test/factories"
     ]
 
   defp elixirc_paths(_), do: ["lib"]
